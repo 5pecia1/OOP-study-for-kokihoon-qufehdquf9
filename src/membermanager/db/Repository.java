1d19d7b9fa04622453d7be2f0068ff8a8a1438be
5pecia1 <pdpxpd@gmail.com>
2018-05-18 04:26:30 +0900

    fix repository return type

--- src/membermanager/db/Repository.java
+++ src/membermanager/db/Repository.java
@@ -1,10 +1,12 @@
 package membermanager.db;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Repository {
-    boolean create(User user);
-    boolean read(User user);
-    boolean delete(User user);
+    User create(User user);
+    List<User> read(User user);
+    User delete(User user);
 }
